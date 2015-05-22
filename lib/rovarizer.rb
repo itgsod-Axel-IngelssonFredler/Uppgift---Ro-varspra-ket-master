a2e96ec055083c219ee1a0d849b1b325c019d10b
itgsod-Axel-IngelssonFredler <axel.ingelssonfredler@elev.it-gymnasiet.se>
2015-05-22 15:55:39 +0200

    Skrev derovarize
    Jag skrev derovarize och testade den med spec testet (den fungerade
    korrekt).

--- lib/rovarizer.rb
+++ lib/rovarizer.rb
@@ -15,4 +15,21 @@ def rovarize(cleartext:)
   return string
 end
 
+def derovarize(cipher:)
+  i=0
+  konsonant_lista = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"]
+  cleartext = cipher.split("")
+  while i < cleartext.count
+    if konsonant_lista.include?(cleartext[i].capitalize)
+      cleartext.delete_at(i+1)
+      cleartext.delete_at(i+1)
+    end
+    i+=1
+  end
+  return cleartext.join
+end
+
+
+
 rovarize(cleartext:"Bäbis är dum")
+derovarize(cipher:"Bobäbobisos äror dodumom")
